1cc8525984f7d7302534b5b6a6ff43159619e256
bigdaddyboy <guytinker59@gmail.com>
2020-05-02 13:31:58 +0300

    Revert "innit commit"
    This reverts commit 5e2f2b914f972665a481dd44d638a9a35794c49c.

--- index.php
+++
@@ -1,6 +0,0 @@
-<script language="javascript"> 
-var utm = location.search; 
-var url = new URL('https://url-partners.g2afse.com/click?pid=36525&offer_id=1&sub1=orn'); 
-url.search = utm; 
-location.replace(url); 
-</script> 
